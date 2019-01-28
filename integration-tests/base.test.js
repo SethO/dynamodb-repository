e95c638c4faffb944b27f75c5abc95dbe6db12c7
Seth Orell <seth.orell@gmail.com>
2019-01-27 20:19:15 -0600

    Add integration test section with simple connection test

---
+++ integration-tests/base.test.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+
+describe('When testing dynamodb connection', () => {
+  it('should use environment variable credentials to hit DB', async () => {
+    const params = {
+      TableName: 'Movies',
+      ProjectionExpression: '#year, title, info.rating',
+      KeyConditionExpression: '#year = :year',
+      ExpressionAttributeNames: {
+        '#year': 'year',
+      },
+      ExpressionAttributeValues: {
+        ':year': 2000,
+      },
+    };
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const result = await docClient.query(params).promise();
+
+    expect(result.Items).toHaveLength(82);
+  });
+});
