d430130ba35048f2d3458c5ba32ff25b33f76dd3
Seth Orell <seth.orell@gmail.com>
2020-09-07 17:37:59 -0500

    Update eslint and fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   extends: [
-      'airbnb-base',
-      'plugin:jest/recommended'
+    'airbnb-base',
+    'plugin:jest/recommended',
   ],
   plugins: [
-      'import',
-      'jest'
+    'import',
+    'jest',
   ],
   env: {
-      node: true,
-      'jest/globals': true,
-      mocha: true
+    node: true,
+    'jest/globals': true,
+    mocha: true,
   },
   rules: {
-      "no-trailing-spaces": [2, {skipBlankLines: true}]
-  }
+    'no-trailing-spaces': [2, { skipBlankLines: true }],
+  },
 };
